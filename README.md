c11d03c0c4ede4e5b4b6b854a4d9576faf8afe6c
Michiel Overtoom <motoom@xs4all.nl>
2014-11-22 09:13:27 +0100

    Exercise API, get tracks and playlist info

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ SoundCloud Audio Uploader for Artists and Record Labels
 Scaup uploads an entire directory of audio files as a 'set' (called 'playlist' in the SoundCloud user interface), and also uploads album art and track metadata.
 
 Since the SoundCloud terms of service only allow applications to authenticate with a password if it's your own account,
-you have to register an app yourself and stick the app 'key' and 'secret' into the configuration file before you can use it.
+you have to register an app yourself and stick the app 'client id' and 'client secret' into the configuration file before you can use it.
--- scaup.py
+++ scaup.py
@@ -1,11 +1,15 @@
+#! /usr/bin/env python
+
 import sys
+import soundcloud # Get it with: pip install soundcloud   Source: https://github.com/soundcloud/soundcloud-python, docs at https://developers.soundcloud.com/docs/api/reference
 
 try:
     import credentials
 except ImportError:
     print """You haven't supplied your credentials yet.
 If you haven't done so, first visit http://soundcloud.com/you/apps/ and register a new application.
-Then, create a file 'credentials.py' with the following lines, filling in the information SoundCloud gave you:
+Then, create a file 'credentials.py' with the following lines, filling in the information SoundCloud gave you,
+and also your SoundCloud username and password:
 
 client_id = "287878237"
 client_secret = "82378233"
@@ -14,4 +18,36 @@ password = "secret"
 
 ...and rerun scaup."""
     sys.exit()
-    
+
+client = soundcloud.Client(
+    client_id=credentials.client_id,
+    client_secret=credentials.client_secret,
+    username=credentials.username,
+    password=credentials.password
+    )
+me = client.get('/me')
+
+def dumpfields(x):
+    fields = x.fields()
+    for k in sorted(fields):
+        print k, fields[k]
+    print
+        
+print "Logged in as %s (#%s)\n" % (me.full_name, me.id)
+
+tracks_in_playlists = set()
+
+playlists = client.get("/playlists", user_id=me.id)
+for playlist in playlists:
+    print "Playlist '%s' (#%s)" % (playlist.title, playlist.id)
+    for track in playlist.tracks:
+        print "    %s (#%s) %d" % (track["title"], track["id"], track["duration"])
+        tracks_in_playlists.add(track["id"])
+    print
+
+print "Tracks which are not in any of your playlists:\n"
+tracks = client.get("/tracks", user_id=me.id)
+for track in tracks:
+    if track.id in tracks_in_playlists:
+        continue
+    print "    '%s' (#%s)" % (track.title, track.id)
